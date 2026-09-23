100d4e3b802413e567afba7761821d45a69e3f43
jw0729 <jw4178@naver.com>
2026-09-23 15:20:16 +0900

    👔List 사용

--- app/src/main/java/kr/ac/kumoh/ce/s20220977/s26w04composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220977/s26w04composable/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -48,25 +49,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    var count1 by remember { mutableStateOf(0)}
-    var count2 by remember { mutableStateOf(0)}
+    val counters = remember {
+        mutableStateListOf(
+            mutableIntStateOf(0),
+            mutableIntStateOf(0)
+        )
+    }
+
+    val modifiers = remember {
+        listOf(
+            Modifier.background(Color(0xFFE8DEF8)),
+            Modifier.background(Color(0XFFE9F680))
+        )
+    }
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
-            Counter(
-                modifer = Modifier.background(Color(0xFFE8DEF8)),
-                count = count1
-            ) {
-                count1 = it
-            }
-
-            Counter(
-                modifer = Modifier.background(Color(0XFFE9F680)),
-                count = count2
-            ) {
-                count2 = it
+            counters.forEachIndexed { index, state ->
+                Counter(
+                    modifier = modifiers[index],
+                    count = state.intValue
+                ) {
+                    state.intValue = it
+                }
             }
         }
     }
