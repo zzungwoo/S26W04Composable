3369547ad78d8c298aa1413b34465d6c105ed28f
jw0729 <jw4178@naver.com>
2026-09-23 14:48:03 +0900

    💄AnimatedVisibility 사용

--- app/src/main/java/kr/ac/kumoh/ce/s20220977/s26w04composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220977/s26w04composable/MainActivity.kt
@@ -4,24 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,7 +59,8 @@ fun MainScreen() {
 
 @Composable
 fun ColumnScope.Counter() {
-    var count by remember { mutableIntStateOf(0)}
+    var count by remember { mutableIntStateOf(0) }
+    var expanded by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -60,7 +68,7 @@ fun ColumnScope.Counter() {
             .padding(8.dp)
             .background(Color(0XFFE9F680)),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = count.toString(),
@@ -73,13 +81,59 @@ fun ColumnScope.Counter() {
             textAlign = TextAlign.Center,
         )
 
-        Button(
-            modifier = Modifier.padding(8.dp).fillMaxWidth(),
-            onClick = {
-                count++
+        Row {
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                onClick = {
+                    count++
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = android.R.drawable.ic_menu_add),
+                    contentDescription = "증가 버튼"
+                )
             }
-        ) {
-            Text("증가", fontSize = 30.sp)
+            Button(
+                modifier = Modifier
+                    .padding(8.dp),
+                onClick = {
+                    expanded = !expanded
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = android.R.drawable.ic_menu_more),
+                    contentDescription = "다른 버튼들"
+                )
+            }
+        }
+
+        AnimatedVisibility(expanded) {
+            Row {
+                Button(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp),
+                    onClick = {
+                        count--
+                        expanded = false
+                    }
+                ) {
+                    Text("감소")
+                }
+                Button(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp),
+                    onClick = {
+                        count = 0
+                        expanded = false
+                    }
+                ) {
+                    Text("초기화")
+                }
+            }
         }
     }
 }
